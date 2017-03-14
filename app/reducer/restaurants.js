c54780131b2f8c32ca58d4759b3933a3f93c2889
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-14 13:37:53 -0500

    RESTAURANTS CRUD

--- app/reducer/restaurants.js
+++ app/reducer/restaurants.js
@@ -1,3 +1,18 @@
-export default function restaurants () {
+import unionBy from 'lodash.unionby';
 
+export default function restaurants(state = [], action) {
+  switch (action.type) {
+    case 'RESTAURANTS@FIND_ALL_COMPLETE':
+      return unionBy(action.data, state, 'id');
+    case 'RESTAURANTS@FIND_ONE_COMPLETE':
+      return unionBy([action.data], state, 'id');
+    case 'RESTAURANTS@CREATE_COMPLETE':
+      return unionBy([action.data], state, 'id');
+    case 'RESTAURANTS@UPDATE_COMPLETE':
+      return unionBy([action.data], state, 'id');
+    case 'RESTAURANTS@DESTROY_COMPLETE':
+      return state.filter(({ id }) => id !== action.id);
+    default:
+      return state;
+  }
 }
--- tests/reducer/restaurants-test.js
+++ tests/reducer/restaurants-test.js
@@ -1,7 +1,67 @@
 import restaurants from '../../app/reducer/restaurants';
 
 module('reducerrestaurants', () => {
-  test('it exists', (assert) => {
-    assert.ok(restaurants, 'restaurants exists');
+  test('RESTAURANTS@FIND_ALL_COMPLETE: with no items', (assert) => {
+    const oldState = [];
+    const action = { type: 'RESTAURANTS@FIND_ALL_COMPLETE', data: [{ id: 1, name: 'Hot Diggity Dog' }] };
+    const expectedState = [{ id: 1, name: 'Hot Diggity Dog' }];
+
+    assert.deepEqual(restaurants(oldState, action), expectedState);
+  });
+
+  test('RESTAURANTS@FIND_ALL_COMPLETE: with new items', (assert) => {
+    const oldState = [{ id: 2, name: 'Brew Works' }];
+    const action = { type: 'RESTAURANTS@FIND_ALL_COMPLETE', data: [{ id: 1, name: 'Hot Diggity Dog' }] };
+    const expectedState = [{ id: 1, name: 'Hot Diggity Dog' }, { id: 2, name: 'Brew Works' }];
+
+    assert.deepEqual(restaurants(oldState, action), expectedState);
+  });
+
+  test('RESTAURANTS@FIND_ALL_COMPLETE: with duplicate ids', (assert) => {
+    const oldState = [{ id: 2, name: 'Brew Works' }];
+    const action = { type: 'RESTAURANTS@FIND_ALL_COMPLETE', data: [{ id: 2, name: 'Hot Diggity Dog' }] };
+    const expectedState = [{ id: 2, name: 'Hot Diggity Dog' }];
+
+    assert.deepEqual(restaurants(oldState, action), expectedState);
+  });
+
+  test('RESTAURANTS@FIND_ONE_COMPLETE: with new id', (assert) => {
+    const oldState = [{ id: 2, name: 'Brew Works' }];
+    const action = { type: 'RESTAURANTS@FIND_ONE_COMPLETE', data: { id: 1, name: 'Hot Diggity Dog' } };
+    const expectedState = [{ id: 1, name: 'Hot Diggity Dog' }, { id: 2, name: 'Brew Works' }];
+
+    assert.deepEqual(restaurants(oldState, action), expectedState);
+  });
+
+  test('RESTAURANTS@FIND_ONE_COMPLETE: with duplicate ids', (assert) => {
+    const oldState = [{ id: 2, name: 'Brew Works' }];
+    const action = { type: 'RESTAURANTS@FIND_ONE_COMPLETE', data: { id: 2, name: 'Hot Diggity Dog' } };
+    const expectedState = [{ id: 2, name: 'Hot Diggity Dog' }];
+
+    assert.deepEqual(restaurants(oldState, action), expectedState);
+  });
+
+  test('RESTAURANTS@CREATE_COMPLETE: with duplicate ids', (assert) => {
+    const oldState = [{ id: 2, name: 'Brew Works' }];
+    const action = { type: 'RESTAURANTS@CREATE_COMPLETE', data: { id: 2, name: 'Hot Diggity Dog' } };
+    const expectedState = [{ id: 2, name: 'Hot Diggity Dog' }];
+
+    assert.deepEqual(restaurants(oldState, action), expectedState);
+  });
+
+  test('RESTAURANTS@UPDATE_COMPLETE', (assert) => {
+    const oldState = [{ id: 2, name: 'Brew Works' }];
+    const action = { type: 'RESTAURANTS@UPDATE_COMPLETE', data: { id: 2, name: 'Hot Diggity Dog' } };
+    const expectedState = [{ id: 2, name: 'Hot Diggity Dog' }];
+
+    assert.deepEqual(restaurants(oldState, action), expectedState);
+  });
+
+  test('RESTAURANTS@DESTROY_COMPLETE', (assert) => {
+    const oldState = [{ id: 2, name: 'Brew Works' }];
+    const action = { type: 'RESTAURANTS@DESTROY_COMPLETE', id: 2 };
+    const expectedState = [];
+
+    assert.deepEqual(restaurants(oldState, action), expectedState);
   });
 });
