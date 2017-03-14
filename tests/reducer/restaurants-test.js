1292f23d635b02fc8fc1f8cfc47313d7cc57129e
Ryan Tablada <ryan.tablada@gmail.com>
2017-03-14 12:35:56 -0500

    Setup Project

---
+++ tests/reducer/restaurants-test.js
@@ -0,0 +1,7 @@
+import restaurants from '../../app/reducer/restaurants';
+
+module('reducerrestaurants', () => {
+  test('it exists', (assert) => {
+    assert.ok(restaurants, 'restaurants exists');
+  });
+});
